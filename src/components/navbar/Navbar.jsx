6867278ff8e1f589391943b66c0192624a81373f
Carlos Guillermo Mejia <Rafikyop@gmail.com>
2023-03-08 15:17:27 -0500

    First commit and change the repository

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="left">
+          <img
+            src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+            alt=""
+          />
+          <span>Homepage</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New and Popular</span>
+          <span>My List</span>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
