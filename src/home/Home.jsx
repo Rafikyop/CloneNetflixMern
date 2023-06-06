e7cb3928cb38bb3a43c0e45d08ef4fc231fd1f9e
Carlos Guillermo Mejia <Rafikyop@gmail.com>
2023-06-05 21:16:35 -0500

    Finishing navbar with useState Scrolled

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -5,6 +5,168 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <img
+        width={"100%"}
+        src="https://lumiere-a.akamaihd.net/v1/images/rafiki-the-lion-king-3_fa285ecf.jpeg"
+        alt="profile img"
+      />
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos vel iste
+      ratione laborum at adipisci eius molestiae. Non fuga blanditiis voluptate
+      vero aut nam animi ea! Ullam provident molestias cum. Lorem, ipsum dolor
+      sit amet consectetur adipisicing elit. Eos vel iste ratione laborum at
+      adipisci eius molestiae. Non fuga blanditiis voluptate vero aut nam animi
+      ea! Ullam provident molestias cum. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Eos vel iste ratione laborum at adipisci eius molestiae.
+      Non fuga blanditiis voluptate vero aut nam animi ea! Ullam provident
+      molestias cum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Eos vel iste ratione laborum at adipisci eius molestiae. Non fuga
+      blanditiis voluptate vero aut nam animi ea! Ullam provident molestias cum.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos vel iste
+      ratione laborum at adipisci eius molestiae. Non fuga blanditiis voluptate
+      vero aut nam animi ea! Ullam provident molestias cum. Lorem, ipsum dolor
+      sit amet consectetur adipisicing elit. Eos vel iste ratione laborum at
+      adipisci eius molestiae. Non fuga blanditiis voluptate vero aut nam animi
+      ea! Ullam provident molestias cum. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Eos vel iste ratione laborum at adipisci eius molestiae.
+      Non fuga blanditiis voluptate vero aut nam animi ea! Ullam provident
+      molestias cum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Eos vel iste ratione laborum at adipisci eius molestiae. Non fuga
+      blanditiis voluptate vero aut nam animi ea! Ullam provident molestias cum.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos vel iste
+      ratione laborum at adipisci eius molestiae. Non fuga blanditiis voluptate
+      vero aut nam animi ea! Ullam provident molestias cum. Lorem, ipsum dolor
+      sit amet consectetur adipisicing elit. Eos vel iste ratione laborum at
+      adipisci eius molestiae. Non fuga blanditiis voluptate vero aut nam animi
+      ea! Ullam provident molestias cum. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Eos vel iste ratione laborum at adipisci eius molestiae.
+      Non fuga blanditiis voluptate vero aut nam animi ea! Ullam provident
+      molestias cum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Eos vel iste ratione laborum at adipisci eius molestiae. Non fuga
+      blanditiis voluptate vero aut nam animi ea! Ullam provident molestias cum.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos vel iste
+      ratione laborum at adipisci eius molestiae. Non fuga blanditiis voluptate
+      vero aut nam animi ea! Ullam provident molestias cum. Lorem, ipsum dolor
+      sit amet consectetur adipisicing elit. Eos vel iste ratione laborum at
+      adipisci eius molestiae. Non fuga blanditiis voluptate vero aut nam animi
+      ea! Ullam provident molestias cum. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Eos vel iste ratione laborum at adipisci eius molestiae.
+      Non fuga blanditiis voluptate vero aut nam animi ea! Ullam provident
+      molestias cum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Eos vel iste ratione laborum at adipisci eius molestiae. Non fuga
+      blanditiis voluptate vero aut nam animi ea! Ullam provident molestias cum.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos vel iste
+      ratione laborum at adipisci eius molestiae. Non fuga blanditiis voluptate
+      vero aut nam animi ea! Ullam provident molestias cum. Lorem, ipsum dolor
+      sit amet consectetur adipisicing elit. Eos vel iste ratione laborum at
+      adipisci eius molestiae. Non fuga blanditiis voluptate vero aut nam animi
+      ea! Ullam provident molestias cum. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Eos vel iste ratione laborum at adipisci eius molestiae.
+      Non fuga blanditiis voluptate vero aut nam animi ea! Ullam provident
+      molestias cum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Eos vel iste ratione laborum at adipisci eius molestiae. Non fuga
+      blanditiis voluptate vero aut nam animi ea! Ullam provident molestias cum.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos vel iste
+      ratione laborum at adipisci eius molestiae. Non fuga blanditiis voluptate
+      vero aut nam animi ea! Ullam provident molestias cum. Lorem, ipsum dolor
+      sit amet consectetur adipisicing elit. Eos vel iste ratione laborum at
+      adipisci eius molestiae. Non fuga blanditiis voluptate vero aut nam animi
+      ea! Ullam provident molestias cum. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Eos vel iste ratione laborum at adipisci eius molestiae.
+      Non fuga blanditiis voluptate vero aut nam animi ea! Ullam provident
+      molestias cum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Eos vel iste ratione laborum at adipisci eius molestiae. Non fuga
+      blanditiis voluptate vero aut nam animi ea! Ullam provident molestias cum.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos vel iste
+      ratione laborum at adipisci eius molestiae. Non fuga blanditiis voluptate
+      vero aut nam animi ea! Ullam provident molestias cum. Lorem, ipsum dolor
+      sit amet consectetur adipisicing elit. Eos vel iste ratione laborum at
+      adipisci eius molestiae. Non fuga blanditiis voluptate vero aut nam animi
+      ea! Ullam provident molestias cum. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Eos vel iste ratione laborum at adipisci eius molestiae.
+      Non fuga blanditiis voluptate vero aut nam animi ea! Ullam provident
+      molestias cum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Eos vel iste ratione laborum at adipisci eius molestiae. Non fuga
+      blanditiis voluptate vero aut nam animi ea! Ullam provident molestias cum.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos vel iste
+      ratione laborum at adipisci eius molestiae. Non fuga blanditiis voluptate
+      vero aut nam animi ea! Ullam provident molestias cum. Lorem, ipsum dolor
+      sit amet consectetur adipisicing elit. Eos vel iste ratione laborum at
+      adipisci eius molestiae. Non fuga blanditiis voluptate vero aut nam animi
+      ea! Ullam provident molestias cum. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Eos vel iste ratione laborum at adipisci eius molestiae.
+      Non fuga blanditiis voluptate vero aut nam animi ea! Ullam provident
+      molestias cum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Eos vel iste ratione laborum at adipisci eius molestiae. Non fuga
+      blanditiis voluptate vero aut nam animi ea! Ullam provident molestias cum.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos vel iste
+      ratione laborum at adipisci eius molestiae. Non fuga blanditiis voluptate
+      vero aut nam animi ea! Ullam provident molestias cum. Lorem, ipsum dolor
+      sit amet consectetur adipisicing elit. Eos vel iste ratione laborum at
+      adipisci eius molestiae. Non fuga blanditiis voluptate vero aut nam animi
+      ea! Ullam provident molestias cum. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Eos vel iste ratione laborum at adipisci eius molestiae.
+      Non fuga blanditiis voluptate vero aut nam animi ea! Ullam provident
+      molestias cum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Eos vel iste ratione laborum at adipisci eius molestiae. Non fuga
+      blanditiis voluptate vero aut nam animi ea! Ullam provident molestias cum.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos vel iste
+      ratione laborum at adipisci eius molestiae. Non fuga blanditiis voluptate
+      vero aut nam animi ea! Ullam provident molestias cum. Lorem, ipsum dolor
+      sit amet consectetur adipisicing elit. Eos vel iste ratione laborum at
+      adipisci eius molestiae. Non fuga blanditiis voluptate vero aut nam animi
+      ea! Ullam provident molestias cum. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Eos vel iste ratione laborum at adipisci eius molestiae.
+      Non fuga blanditiis voluptate vero aut nam animi ea! Ullam provident
+      molestias cum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Eos vel iste ratione laborum at adipisci eius molestiae. Non fuga
+      blanditiis voluptate vero aut nam animi ea! Ullam provident molestias cum.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos vel iste
+      ratione laborum at adipisci eius molestiae. Non fuga blanditiis voluptate
+      vero aut nam animi ea! Ullam provident molestias cum. Lorem, ipsum dolor
+      sit amet consectetur adipisicing elit. Eos vel iste ratione laborum at
+      adipisci eius molestiae. Non fuga blanditiis voluptate vero aut nam animi
+      ea! Ullam provident molestias cum. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Eos vel iste ratione laborum at adipisci eius molestiae.
+      Non fuga blanditiis voluptate vero aut nam animi ea! Ullam provident
+      molestias cum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Eos vel iste ratione laborum at adipisci eius molestiae. Non fuga
+      blanditiis voluptate vero aut nam animi ea! Ullam provident molestias cum.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos vel iste
+      ratione laborum at adipisci eius molestiae. Non fuga blanditiis voluptate
+      vero aut nam animi ea! Ullam provident molestias cum. Lorem, ipsum dolor
+      sit amet consectetur adipisicing elit. Eos vel iste ratione laborum at
+      adipisci eius molestiae. Non fuga blanditiis voluptate vero aut nam animi
+      ea! Ullam provident molestias cum. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Eos vel iste ratione laborum at adipisci eius molestiae.
+      Non fuga blanditiis voluptate vero aut nam animi ea! Ullam provident
+      molestias cum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Eos vel iste ratione laborum at adipisci eius molestiae. Non fuga
+      blanditiis voluptate vero aut nam animi ea! Ullam provident molestias cum.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos vel iste
+      ratione laborum at adipisci eius molestiae. Non fuga blanditiis voluptate
+      vero aut nam animi ea! Ullam provident molestias cum. Lorem, ipsum dolor
+      sit amet consectetur adipisicing elit. Eos vel iste ratione laborum at
+      adipisci eius molestiae. Non fuga blanditiis voluptate vero aut nam animi
+      ea! Ullam provident molestias cum. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Eos vel iste ratione laborum at adipisci eius molestiae.
+      Non fuga blanditiis voluptate vero aut nam animi ea! Ullam provident
+      molestias cum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Eos vel iste ratione laborum at adipisci eius molestiae. Non fuga
+      blanditiis voluptate vero aut nam animi ea! Ullam provident molestias cum.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos vel iste
+      ratione laborum at adipisci eius molestiae. Non fuga blanditiis voluptate
+      vero aut nam animi ea! Ullam provident molestias cum. Lorem, ipsum dolor
+      sit amet consectetur adipisicing elit. Eos vel iste ratione laborum at
+      adipisci eius molestiae. Non fuga blanditiis voluptate vero aut nam animi
+      ea! Ullam provident molestias cum. Lorem, ipsum dolor sit amet consectetur
+      adipisicing elit. Eos vel iste ratione laborum at adipisci eius molestiae.
+      Non fuga blanditiis voluptate vero aut nam animi ea! Ullam provident
+      molestias cum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Eos vel iste ratione laborum at adipisci eius molestiae. Non fuga
+      blanditiis voluptate vero aut nam animi ea! Ullam provident molestias cum.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos vel iste
+      ratione laborum at adipisci eius molestiae. Non fuga blanditiis voluptate
+      vero aut nam animi ea! Ullam provident molestias cum.
     </div>
   );
 };
